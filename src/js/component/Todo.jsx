75fd72aadefa8e442a89cea63b289f553e51feeb
riveromj <marigen@gmail.com>
2020-11-26 11:37:00 +0000

    first commit

---
+++ src/js/component/Todo.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "../../styles/todo.css";
+
+function Todo(props) {
+	return (
+		<li className="list-group-item">
+			{props.id + 1}. {props.todo}{" "}
+			<span className="delete" onClick={() => props.deleteTodo(props.id)}>
+				{" "}
+				X{" "}
+			</span>
+		</li>
+	);
+}
+
+Todo.propTypes = {
+	todo: PropTypes.string,
+	id: PropTypes.number,
+	deleteTodo: PropTypes.func
+};
+
+export default Todo;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Todo from "./Todo.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+	const [todo, setTodo] = useState("");
+	const [listTodo, setListTodo] = useState([]);
 
-//create your first component
-export function Home() {
+	const deleteTodo = id => {
+		const list = [...listTodo];
+		list.splice(id, 1);
+		setListTodo(list);
+		console.log("borrar");
+	};
+
+	const handelKeyDow = e => {
+		if (e.keyCode == 13 && event.target.value != "") {
+			setListTodo([...listTodo, todo]);
+			setTodo("");
+		}
+	};
+
+	/*useEffect(
+		() => {
+			console.log("CICLO DE VIDA");
+		},
+		[listTodo]
+	);*/
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="card container">
+			<h1>TO dO LIST</h1>
+			<input
+				className="todo"
+				value={todo}
+				type="text"
+				placeholder="What needs to be done"
+				onChange={e => setTodo(e.target.value)}
+				onKeyDown={handelKeyDow}
+				tabIndex="13"
+			/>
+			<ul className="list-group">
+				{listTodo.map((todo, index) => {
+					return (
+						<Todo
+							key={index}
+							todo={todo}
+							id={index}
+							deleteTodo={deleteTodo}
+						/>
+					);
+				})}
+			</ul>
+			<div className="card-footer">{listTodo.length} Item</div>
 		</div>
 	);
 }
+
+export default Home;
